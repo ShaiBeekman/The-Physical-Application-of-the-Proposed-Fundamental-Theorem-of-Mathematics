399b8af2ae698e7af453e4fb5f2d77888e47dac9
Shai Beekman <shaibeekman@gmail.com>
2024-07-08 01:46:17 -0400

    added Exists.java
    added object.java
    
    added p2.java

---
+++ object.java
@@ -0,0 +1,7 @@
+public class object {
+
+List<Object> list = new ArrayList<>();
+list.add("Hello");
+        list.add(123);
+        list.add(new Date());
+        }//*/a/*//
---
+++ p2.java
@@ -0,0 +1,14 @@
+public class p2 { //https://docs.google.com/document/d/1I3Za5EmjttAJeufZtcCmvBO9-W6zgU-5tEZctxb0jsY/edit
+    //On a theory of absolute potential existence
+
+
+    //Given some non-existence H, there exists some existence E1, E2, E3, ...En, ...EM, such that E1, E2, E3, ... En, ..., EM implies the existence of H.
+
+    //what does this mean...
+
+    well
+
+
+
+
+}
